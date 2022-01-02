b4c92b0e2d0a77bcb9ff218cf8320c58e7c3baa1
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2022-01-02 18:35:59 +0100

    :bug: Access to Button's #title is needed

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+# Button component
 class ButtonComponent < ViewComponent::Base
+  attr_accessor :title
 
   def initialize(title:)
+    super(title)
     @title = title
   end
-
 end
